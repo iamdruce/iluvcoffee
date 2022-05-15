54af535cd4ceb75f338fea3cb20fb38426f24f3b
iamdruce <iamdruce@gmail.com>
2022-05-15 12:33:39 +0300

    finished chapter 5

---
+++ src/events/entities/event.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+
+
+@Index(['type', 'name'])
+@Entity()
+export class Event {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  type: string;
+
+  @Index()
+  @Column()
+  name: string;
+
+  @Column('json')
+  payload: Record<string, any>
+}
